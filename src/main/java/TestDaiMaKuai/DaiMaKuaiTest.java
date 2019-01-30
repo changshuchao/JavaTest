635e40eabe4e1c94dab085d7993c4b2819832c61
changshuchao <1607780574@qq.com>
2019-01-30 22:02:02 +0800

    fixuo

--- src/main/java/TestDaiMaKuai/DaiMaKuaiTest.java
+++ src/main/java/TestDaiMaKuai/DaiMaKuaiTest.java
@@ -17,7 +17,6 @@ public class DaiMaKuaiTest {
     }
 
     public static void main(String[] args) {
-        
         System.out.println("2普通代码块");
 
         /*在new一个对象的时候总是先执行构造代码，再执行构造函数，但是有一点需要注意构造代码不是在构造函数之前运行的，它是依托构造函数执行的。*/
